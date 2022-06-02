4c979e46bcb7aa0c68961fca750b9404e5f61f85
Vitor Andrade <andrade.vitoralmeida@gmail.com>
2022-06-01 23:44:03 -0300

    rota get / adicionada

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 
 server.post('/', (req, res, next) => {console.log(req.body); res.status(200); return})
+server.get('/', (req, res, next) => {res.send(req.query); return})
 
 
 server.listen(5000);
