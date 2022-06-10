55f5510f09411f37f529e0a69811dd705215ab62
Vitor Andrade <andrade.vitoralmeida@gmail.com>
2022-06-10 07:06:46 -0300

    engine ejs adicionada

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const server = express();
 server.use(express.urlencoded({ extended: true}));
 const bodyParser = require('body-parser');
 
+server.set('view engine','ejs');
+server.set('views', path.join(__dirname, "views"));
+
+
 server.post('/', (req, res, next) => {console.log(req.body); res.redirect('/'); return})
 server.get('/', (req, res, next) => {res.render('index'); return})
 
